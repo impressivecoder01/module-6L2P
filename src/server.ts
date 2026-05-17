6d017346749cdf8bf149391ab99b4ee5df84f11f
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-18 03:33:21 +0600

    class tw0

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,23 @@
 import { createServer, IncomingMessage, Server } from "http";
 
 const server :Server = createServer((req :IncomingMessage, res)=> {
-    console.log(req);
+    // console.log(req.url); 
+    // console.log(req.method);
+    const url = req.url
+    const method = req.method
+    if(url === '/' && method === "GET"){
+        // console.log('this is root');
+        res.writeHead(200,{'content-type': 'application/json'})
+        res.end(JSON.stringify({message: "this is root"}))
+    }
+    else if(url?.startsWith('/products')){
+        res.writeHead(200,{'content-type': 'application/json'})
+        res.end(JSON.stringify({message: "this is product route"}))
+    }
+    else{
+        res.writeHead(404,{'content-type': 'application/json'})
+        res.end(JSON.stringify({message: "this route is not here"}))
+    }
 })
 
 const port = 3000
