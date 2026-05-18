aec6190948eacbea7cacce77504bce4582124dca
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-19 02:49:40 +0600

    error solve

--- src/controller/products.controller.ts
+++ src/controller/products.controller.ts
@@ -4,9 +4,17 @@ import { readProduct } from "../service/product.service";
 export const productController = (req: IncomingMessage, res:ServerResponse) => {
     const url = req.url
     const method = req.method
+
+    const urlParts = url?.split("/")
+    const id = urlParts && urlParts[1] === 'products' ? Number(urlParts[2]) : null
+    console.log(id,);
+    // get all products
     if(url === '/products' && method === "GET"){
        const products =  readProduct()
     res.writeHead(200,{'content-type': 'application/json'})
     res.end(JSON.stringify({message: "Products retrieved successfully", data: products}))
+    }
+    else if(method === 'GET' ){
+
     }
 }
--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -1,7 +1,7 @@
 import path from "path"
 import fs from 'fs'
 
-const filePath = path.join(process.cwd(), "src", "service", "product.service.ts");
+const filePath = path.join(process.cwd(), "src", "database", "db.json");
 
 
 export const readProduct = () => {
