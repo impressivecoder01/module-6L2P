f7f7071f97bba71003cc10f24923fd56907c3c5e
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-19 23:36:47 +0600

    product updated

--- src/controller/products.controller.ts
+++ src/controller/products.controller.ts
@@ -35,4 +35,18 @@ export const productController = async (req: IncomingMessage, res:ServerResponse
     res.writeHead(200,{'content-type': 'application/json'})
     res.end(JSON.stringify({message: "Product created successfully", data: newProduct}))
     }
+    else if(method === "PUT" && id !== null){
+        const body = await parseBody(req)
+        const products = readProduct()
+        const index = products.findIndex((p: IProduct)=> p.id === id)
+        // console.log(index);
+        if(index < 0){
+            res.writeHead(404,{'content-type': 'application/json'})
+            res.end(JSON.stringify({message: "Product not found", data: null}))
+        }
+        products[index] = {id: products[index].id, ...body}
+        insertProduct(products)
+         res.writeHead(200,{'content-type': 'application/json'})
+            res.end(JSON.stringify({message: "Product updated successfully", data: products[index]}))
+    }
 }
